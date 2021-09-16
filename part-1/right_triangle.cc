7faca7b39ea1e0716692a390841450c2f013d54e
gracelee2 <48224051+gracelee2@users.noreply.github.com>
2021-09-16 13:14:17 -0700

    Update right_triangle.cc

--- part-1/right_triangle.cc
+++ part-1/right_triangle.cc
@@ -1,6 +1,6 @@
 // Grace Lee
 // CPSC 120-01
-// 2021-09-09
+// 2021-09-15
 // grace1@csu.fullerton.edu
 // @gracelee2
 //
@@ -19,22 +19,16 @@ using namespace std;
 int main(int argc, char const *argv[]) {
 
 
-  // Remember that the Pythagorean Theorem states that given a right triangle (a
-  // triangle with one 90 degree angle), the sum of the squares of its sides sum
-  // to the square of the hypotenuse. That means we can express the theorem as
-  // the equality: (side_a * side_a) + (side_b * side_b) == (side_c * side_c)
-  // The left hand side represents the sum of the squares of the side.
-  // The right hand side represents the square of the hypotenuse.
 
 
-  // Remember to print a space after the question mark but no carriage return.
+  
   cout << "What's the length of the first side?\n";
 
   int side_a = 0;
 
   cin >> side_a;
 
-  // Remember to print a space after the question mark but no carriage return.
+  
   cout << "What's the length of the second side?\n";
 
   
@@ -42,20 +36,17 @@ int main(int argc, char const *argv[]) {
 
   cin >> side_b;
 
-  // Remember to print a space after the question mark but no
-  // carriage return.
+  
   cout << "What's the length of the diagonal or hypontenuse?\n";
 
   int side_c = 0;
 
   cin >> side_c;
-  // Declare a variable named left_hand_side to represent the left hand side of
-  // our equality
+  
   int left_hand_side = (side_a * side_a) + (side_b * side_b);
 
 
-  // Declare a variable named right_hand_side to represent the right hand side
-  // of our equality
+  
   int right_hand_side = side_c * side_c;
 
   cout << "You entered " << side_a << " for the length of the first side.\n";
