475867113717ac2b8c7e3552fe16b5fd2c9f1193
gracelee2 <48224051+gracelee2@users.noreply.github.com>
2021-09-16 13:54:28 -0700

    Update dhms.cc

--- part-3/dhms.cc
+++ part-3/dhms.cc
@@ -15,32 +15,32 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  
+
   const int knumsecmin = 60;
-  
+
   const int knumsechour = 3600;
-  
+
   const int knumsecday = 86400;
-  
+
   int number_seconds = 0;
-  
+
   cout << "Please enter the number of seconds you'd like to convert:";
-  
+
   cin >> number_seconds;
-  
+
   int number_days = number_seconds / knumsecday;
 
-  
+
   number_seconds = number_seconds % knumsecday;
-  
+
   int number_hours = number_seconds / knumsechour;
-  
+
   number_seconds = number_seconds % knumsechour;
-  
+
   int number_minutes = number_seconds / knumsecmin;
-  
+
   number_seconds = number_seconds % knumsecmin;
-  
+
   cout << "That's " << number_days << " days, " << number_hours << " hours, "
        << number_minutes << " minutes,and " << number_seconds << " seconds.\n";
   return 0;
